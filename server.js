6eb091a2926492120c9d9f357dce1d6448032075
vikash54321 <vikashchoudhary1012@gmail.com>
2017-02-14 20:47:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-  app.grt('/article-one',function (req,res) {  
+app.get('/article-one',function (req, res) {  
       res.send('Article one requested and will be served here');
 });
 
-  app.grt('/article-two',function (req,res) {  
+app.get('/article-two',function (req, res) {  
       res.send('Article two requested and will be served here');
 });
 
-  app.grt('/article-three ',function (req,res) {  
+app.get('/article-three ',function (req, res) {  
       res.send('Article three requested and will be served here');
 });
 
