181c9d7a77a12174ba43d5f419129aff6486b8f8
vikash54321 <vikashchoudhary1012@gmail.com>
2017-02-15 00:07:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ app.use(morgan('combined'));
 var articleOne ={
   title: 'Article one | Vikash kumar choudhary ',
   heading:'Article one'.
-  date: 'December 10,1996'
-  content: <p>
-                This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.
+  `Date : "December 10, 1996"
+  content: 
+          <p>
+             `  This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.
             </p>
             <p>
                 This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.This is  vikash first article and i am learning HTML.
