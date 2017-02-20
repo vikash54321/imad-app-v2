f1122288a70823fe714ba3f1d1626a5e2fad1361
vikash54321 <vikashchoudhary1012@gmail.com>
2017-02-20 20:53:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,6 +103,16 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req, res) { // URL: /submit-name?name=xxxxx
+    // Get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    // JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res) {
     // articleName == article-one
    // articles[articleName] == {} content object for article one
@@ -120,16 +130,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name/:name', function(req, res) {
-    // Get the name from the request
-    var name = req.params.name;
-    
-    names.push(name);
-    // JSON: Javascript Object Notation
-    res.send(JSON.stringify(names));
 });
 
 
