3901842d6d99072761ee33b6abc3460e3b915a71
vikash54321 <vikashchoudhary1012@gmail.com>
2017-02-20 14:26:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,12 @@ element.innerHTML = 'Vikash';
 
 // Move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft +10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
 };
     
