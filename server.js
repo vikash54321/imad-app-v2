a034a6e7d0a2d0122d1000adcbdc413ff6c2ef73
vikash54321 <vikashchoudhary1012@gmail.com>
2017-02-28 00:10:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,22 +136,22 @@ app.get('/submit-name', function(req, res) { // URL: /submit-name?name=xxxxx
 });
 
 app.get('/articles/:articleName', function (req, res) {
-    // articleName == article-one
+   // articleName == article-one
    // articles[articleName] == {} content object for article one
    
    // SELECT * FROM article WHERE title = 'article-one' 
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
-        if (err) {
-            res.status(500).send(err.toString());
+   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
         } else {
-            if (result.rows.length === 0) {
-                res.status(404).send('Article not found');
-            } else {
-              var articleData = result.rows[0]; 
-              res.send(createTemplate(articleData));
-              }
-            }
-        });
+            var articleData = result.rows[0]; 
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
 });
 
 app.get('/ui/style.css', function (req, res) {
