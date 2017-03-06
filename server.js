463e987aa1f471007601b9d4c8db54135fc922e7
vikash54321 <vikashchoudhary1012@gmail.com>
2017-03-07 00:31:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ var articles = {
 			            ${heading}
 			        </h3>
 			        <div>
-			        ${Date}
+			        ${Date.toDateString()}
 			        </div>
 			        <div>
 			        ${content}
