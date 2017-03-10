1510a90e71932915218cc052827368a8b1f8064f
vikash54321 <vikashchoudhary1012@gmail.com>
2017-03-10 11:25:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/', function (req, res) {
 function hash (input, salt) {
     // How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+    return hashed.toString('hex');
 }
 
 
