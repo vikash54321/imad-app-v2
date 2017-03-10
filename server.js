0e8d3b287f379644fe60cbaee79d8332e43b5a5a
vikash54321 <vikashchoudhary1012@gmail.com>
2017-03-10 19:28:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'vikash54321',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 			
 var articles = {
 'article-one':  {
@@ -155,7 +160,18 @@ app.post('/login',  function (req, res) {
                     var salt = dbString.split('$')[2];
                     var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
                     if (hashedPassword === dbString) {
+                        
+                        // Set the session
+                        req.session.auth = {userId: result.rows[0].id};
+                        // set cookie with a session id
+                        // internally on the server side, it maps the session id to an object
+                        // { auth: {userId }}
+                        
+                        
                         res.send('credentials correct!');
+                        
+                        
+                        
                     } else {
                         res.send(403).send('username/password is invalid');
                         
@@ -165,6 +181,13 @@ app.post('/login',  function (req, res) {
     });
 });
 
+app.get('/check-login', function (req, res) {
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        res.send('You are logged in: ' + req.session.auth.userId.toString());
+    } else {
+        res.send('You are not logged in');
+    }
+    });
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
